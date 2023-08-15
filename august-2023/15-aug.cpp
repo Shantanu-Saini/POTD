d83b348c1c0e6d6b32b4a5971fbb7e7fcdd19e75
Shantanu <shantanusaini7000@gmail.com>
2023-08-15 13:58:44 +0530

    15 August

---
+++ august-2023/15-aug.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    int maxOnes(int a[], int n)
+    {
+        // Your code goes here
+        int ones = 0;
+        for(int i = 0; i < n; i++){
+            if(a[i]==0){
+                a[i]=1;
+            }
+            else{
+                a[i] = -1;
+                ones++;
+            }
+        }
+        
+        int sum = 0;
+        long maxi =0;
+        for(int i = 0; i<n; i++){
+            sum += a[i];
+            if(maxi<sum){
+                maxi = sum;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        return (maxi+ones);
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n+5];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        cout<< ob.maxOnes(a, n) <<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
