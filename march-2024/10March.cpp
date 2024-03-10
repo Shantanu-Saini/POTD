ce74e409c33202c7f23fe52113d304d4abcfb186
Shantanu <shantanusaini7000@gmail.com>
2024-03-10 11:47:41 +0530

    Remove all duplicates from a given string

---
+++ march-2024/10March.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+class Solution
+{
+public:
+    string removeDuplicates(string str)
+    {
+        // code here
+        string ans = "";
+        int l = str.length();
+        unordered_map<char, int> mp;
+
+        for (int i = 0; i < l; i++)
+        {
+            mp[str[i]]++;
+            if (mp[str[i]] == 1)
+            {
+                ans += str[i];
+            }
+        }
+
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string str;
+        cin >> str;
+        Solution ob;
+        auto ans = ob.removeDuplicates(str);
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
