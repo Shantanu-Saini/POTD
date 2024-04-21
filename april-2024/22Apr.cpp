9021329e75857c5f5e39d4999cba360e2c5d7957
Shantanu <shantanusaini7000@gmail.com>
2024-04-22 02:05:17 +0530

    Row with minimum number of 1's

---
+++ april-2024/22Apr.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    int minRow(int n, int m, vector<vector<int>> a)
+    {
+        // code here
+        int mini = INT_MAX;
+        int row = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int rowSum = 0;
+            for (int j = 0; j < m; j++)
+            {
+                rowSum += a[i][j];
+            }
+            if (rowSum < mini)
+            {
+                mini = rowSum;
+                row = i + 1;
+            }
+        }
+
+        return row;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N, M;
+        cin >> N >> M;
+        vector<vector<int>> A(N, vector<int>(M));
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                cin >> A[i][j];
+            }
+        }
+        Solution ob;
+        cout << ob.minRow(N, M, A) << "\n";
+    }
+}
+// } Driver Code Ends
